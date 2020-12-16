ed9a1753fa3ab87b529c444d8fcb125a3212a734
shakil2995 <shakil2995@gmail.com>
2020-12-16 16:54:49 +0600

    base

---
+++ cmake-build-debug/CMakeFiles/Cpp_playground.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_playground.dir/Multidimentional_Arrays.cpp.obj"
+  "Cpp_playground.exe"
+  "Cpp_playground.exe.manifest"
+  "Cpp_playground.pdb"
+  "libCpp_playground.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_playground.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
