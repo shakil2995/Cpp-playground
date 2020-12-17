91f3fdbee4594d3e7fbe089a4d0400ed482bdda2
shakil2995 <shakil2995@gmail.com>
2020-12-18 03:13:09 +0600

    Update main.cpp

--- Reference/main.cpp
+++ Reference/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int x;
+    float y;
+    string z;
+    cin>>x>>y>>z;
+    cout<<x<<" "<<y<<" "<<z<<" ";
     return 0;
 }
